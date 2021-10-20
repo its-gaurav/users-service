c48b7c2fa92b15b6b271766f55cf674cd142dbfb
Gaurav Raj <gauravraj911@gmail.com>
2021-10-21 00:27:04 +0530

    adding order-history

--- src/main/java/com/gaurav/users/domains/OrderHistory.java
+++ src/main/java/com/gaurav/users/domains/OrderHistory.java
@@ -1,11 +1,14 @@
 package com.gaurav.users.domains;
 
+import java.math.BigDecimal;
+
 public class OrderHistory {
 
     private Long id;
-    private String orderId;
+    private String orderNumber;
     private Long restaurantId;
     private String restaurantName;
+    private BigDecimal totalAmount;
     private Long itemId;
 
 }
